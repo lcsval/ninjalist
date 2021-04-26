a477967835d1392900a1c49bb0cbead45b800988
Lucas Val <lucas_val@outlook.com>
2021-04-26 19:11:05 -0300

    Adding other components

---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+const Navar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>Ninja List</h1>
+            </div>
+
+            <a>Home</a>
+            <a>AboutHome</a>
+            <a>Ninja Listing</a>
+        </nav>
+    );
+}
+ 
+export default Navar;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita similique quas id cum! Optio quos quaerat reiciendis ad aliquam eum libero harum labore nostrum voluptate mollitia voluptatibus, illo, dolorum fugit.</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel id distinctio accusamus ullam voluptas maxime nostrum tempora cum quos, inventore eligendi pariatur explicabo voluptates sint. Earum deleniti ipsam minima nisi.</p>
+      <Footer></Footer>
     </div>
   )
 }
