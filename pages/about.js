b934926ea80ebcf8c4dcc5ec50f4051651e7a005
Lucas Val <lucas_val@outlook.com>
2021-04-26 20:07:07 -0300

    Images e Metadata

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from 'next/head'
+
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate possimus, fugit totam eveniet natus error velit facilis cupiditate deleniti, ut dicta ipsam explicabo reprehenderit animi nesciunt delectus maiores pariatur incidunt.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate possimus, fugit totam eveniet natus error velit facilis cupiditate deleniti, ut dicta ipsam explicabo reprehenderit animi nesciunt delectus maiores pariatur incidunt.</p>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate possimus, fugit totam eveniet natus error velit facilis cupiditate deleniti, ut dicta ipsam explicabo reprehenderit animi nesciunt delectus maiores pariatur incidunt.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate possimus, fugit totam eveniet natus error velit facilis cupiditate deleniti, ut dicta ipsam explicabo reprehenderit animi nesciunt delectus maiores pariatur incidunt.</p>
+            </div>
+        </>
     );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,19 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div >
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita similique quas id cum! Optio quos quaerat reiciendis ad aliquam eum libero harum labore nostrum voluptate mollitia voluptatibus, illo, dolorum fugit.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel id distinctio accusamus ullam voluptas maxime nostrum tempora cum quos, inventore eligendi pariatur explicabo voluptates sint. Earum deleniti ipsam minima nisi.</p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div >
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita similique quas id cum! Optio quos quaerat reiciendis ad aliquam eum libero harum labore nostrum voluptate mollitia voluptatibus, illo, dolorum fugit.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel id distinctio accusamus ullam voluptas maxime nostrum tempora cum quos, inventore eligendi pariatur explicabo voluptates sint. Earum deleniti ipsam minima nisi.</p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   )
 }
