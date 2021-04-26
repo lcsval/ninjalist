cdeb1462e0a348c9c97080c1511a74910c4b11ef
Lucas Val <lucas_val@outlook.com>
2021-04-26 20:15:39 -0300

    Fetching data getstaticprops

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,26 @@
-const Ninjas = () => {
+import style from '../../styles/Ninjas.module.css'
+
+export const getStaticProps = async () => {
+
+    const res = await fetch('http://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: { ninjas: data }
+    }
+}
+
+const Ninjas = ({ ninjas }) => {
     return ( 
         <div>
             <h1>All Ninjas</h1>
+            {ninjas.map(ninja => (
+                <div key={ninja.id}>
+                    <a className={style.single}>
+                        <h3>{ ninja.name }</h3>
+                    </a>
+                </div>
+            ))}
         </div>
     );
 }
