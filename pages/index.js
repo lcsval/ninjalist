076fa7c8181000a054b159fa8991f7ce8f5d6804
Lucas Val <lucas_val@outlook.com>
2021-04-26 18:58:38 -0300

    Pages and Routes

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita similique quas id cum! Optio quos quaerat reiciendis ad aliquam eum libero harum labore nostrum voluptate mollitia voluptatibus, illo, dolorum fugit.</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel id distinctio accusamus ullam voluptas maxime nostrum tempora cum quos, inventore eligendi pariatur explicabo voluptates sint. Earum deleniti ipsam minima nisi.</p>
     </div>
   )
 }
